ad35e3add0a6cf531d38360ca8105a2cdeb440af
SyedaWania23 <163909534+SyedaWania23@users.noreply.github.com>
2024-11-01 15:06:38 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div>
+      <nav className="flex gap-14  w-full bg-blue-700 h-14 text-white">
+      <div className="bg-purple-700 h-14 p-3"><h1>Logo</h1></div>
+
+      <div><p className="font-bold  p-4">Tuition Free Education Program on Latest Technologies</p></div>
+      <div>
+        <ul className="flex p-4 gap-16 justify-end">
+          <li><a href="">Home</a>
+          </li>
+          <li><a href="">Apply</a></li>
+          <li><a href="">Jobs</a></li>
+          <li><a href="">Results</a></li>
+          <li><a href="">Courses</a></li>
+        </ul>
+      </div>
+      </nav>
+      
+    
+      
+    </div>
+
+  );
+}
+
